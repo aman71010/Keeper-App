5f455dd5fbe01c0e90b4b6b046d3949d4682f913
Aman <amangupta71010@gmail.com>
2021-07-27 01:26:17 +0530

    deploy app on github

---
+++ build/precache-manifest.512d885a3c539d97bc756fb24fa55a12.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a511d065186889d13cf4d46e7e5029f5",
+    "url": "/Notes-Keeper-app/index.html"
+  },
+  {
+    "revision": "5097cc4a7f6d1ffca7af",
+    "url": "/Notes-Keeper-app/static/js/2.87d000ba.chunk.js"
+  },
+  {
+    "revision": "64279d5dd3dea7d30f5f",
+    "url": "/Notes-Keeper-app/static/js/main.2e339443.chunk.js"
+  },
+  {
+    "revision": "2eafa161a15ed9d6ac5e",
+    "url": "/Notes-Keeper-app/static/js/runtime-main.9b1f2389.js"
+  }
+]);
